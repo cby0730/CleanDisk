faa15df006940de111989947d855885866f1ba66
Billy <ab26100630@gmail.com>
2025-08-19 11:14:22 +0800

    Initial Commit

---
+++ CleanDisk/CleanDiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanDiskApp.swift
+//  CleanDisk
+//
+//  Created by 陳柏宇 on 2025/8/19.
+//
+
+import SwiftUI
+
+@main
+struct CleanDiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanDiskTests/CleanDiskTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanDiskTests.swift
+//  CleanDiskTests
+//
+//  Created by 陳柏宇 on 2025/8/19.
+//
+
+import Testing
+@testable import CleanDisk
+
+struct CleanDiskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
